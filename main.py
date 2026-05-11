abb87ee66fd335c2a8d58dd960613b2ff6858383
Rubin J2W <rubin.kumar@joulestowatts.com>
2026-05-11 21:20:45 +0530

    cors added for FE domain

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ app.add_middleware(
         "http://localhost:8081",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:8081",
+        "https://macromaster-suite.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
