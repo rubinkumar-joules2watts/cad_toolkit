1c2df0ed1319ff18e2084c7ffaf890169f2b1e9a
Rubin J2W <rubin.kumar@joulestowatts.com>
2026-05-11 21:57:04 +0530

    Added CORS

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ app.add_middleware(
         "http://localhost:8081",
         "http://127.0.0.1:8080",
         "http://127.0.0.1:8081",
-        "https://macromaster-suite.vercel.app/"
+        "https://macromaster-suite.vercel.app/",
+        "http://140.238.227.29:5173/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
