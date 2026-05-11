f487b182ebf63b792929c70bb6811700ebae7926
Rubin J2W <rubin.kumar@joulestowatts.com>
2026-05-11 22:07:36 +0530

    Added CORS

--- main.py
+++ main.py
@@ -11,31 +11,12 @@ def read_root() -> dict[str, str]:
 
 app = FastAPI(title="MACROS Toolkit")
 
-ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://localhost:8081",
-    "http://127.0.0.1:8080",
-    "http://127.0.0.1:8081",
-    "https://macromaster-suite.vercel.app",
-    "http://140.238.227.29:5173",
-]
-
-ALLOWED_METHODS = ["GET", "POST", "OPTIONS"]
-
-ALLOWED_HEADERS = [
-    "Accept",
-    "Accept-Language",
-    "Content-Language",
-    "Content-Type",
-    "Authorization",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=ALLOWED_METHODS,
-    allow_headers=ALLOWED_HEADERS,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
